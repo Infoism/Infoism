d637c733f19a3690b528a407ea6022d772495230
pidanmeng <1092333914@qq.com>
2023-02-08 15:50:24 +0800

    fix: 🐛 vite

--- apps/infoism/electron.vite.config.ts
+++ apps/infoism/electron.vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
 import { ArcoResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   main: {
@@ -23,7 +22,6 @@ export default defineConfig({
     plugins: [
       vue(),
       unocss(),
-      vueJsx({}),
       Components({
         resolvers: [
           ArcoResolver({
